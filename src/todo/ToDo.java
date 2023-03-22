860071c87366e247f487cbd9a81db76e75966bd1
dorjazjargal <dorjazjargal@gmail.com>
2023-03-22 11:51:41 +0800

    list method nemsen

--- src/todo/ToDo.java
+++ src/todo/ToDo.java
@@ -19,6 +19,7 @@ public class ToDo {
      */
     static String str, command, toDO;
     static ArrayList<String> my_toDo = new ArrayList<String>();
+    static Integer Size_arr = 0;
 
     public static void main(String[] args) {
         // TODO code application logic here
@@ -27,44 +28,63 @@ public class ToDo {
         while (true) {
             str = sc.nextLine();
             String[] inputArray = str.split("\\s+");
-            
+            Size_arr = inputArray.length;
             if (str != null) {
                 command = inputArray[0];
                 command = command.toLowerCase();
             }
-            
-            if(inputArray.length==1 && command.equals("list")){
-               list_console();
+
+            if (inputArray.length == 1 && command.equals("list")) {
+                List_console();
             }
-            
+
             if (command.equals("add") || command.equals("done") || command.equals("list")) {
                 if (command.equals("add")) {
-                    toDO = trim_add(str);
+                    toDO = Trim_add(str);
                     my_toDo.add(toDO);
 //                    System.out.println(my_toDo.toString());
                     System.out.println("Created task " + my_toDo.size());
                 }
-//                if (command.equals("list")) {
-//                    list_console();
-//                }
+                if (command.equals("done") && Size_arr >= 2) {
+                    Done(inputArray[1]);
+                }
             } else {
                 System.out.println("Invalid command");
             }
         }
     }
 
-    public static String trim_add(String str2) {
+    public static String Trim_add(String str2) {
         str2 = (String) str2.subSequence(4, str2.length());
         return str2;
     }
 
-    public static String list_console() {
+    public static String List_console() {
         System.out.println("ID Description");
-        System.out.println("_ _  _ _ _ _ _ "+"\n");
-        for(int i=0; i<my_toDo.size();i++){
-            System.out.println(my_toDo.indexOf(my_toDo.get(i))+1+" "+my_toDo.get(i));
+        System.out.println("_ _  _ _ _ _ _ " + "\n");
+        for (int i = 0; i < my_toDo.size(); i++) {
+            System.out.println(my_toDo.indexOf(my_toDo.get(i)) + 1 + " " + my_toDo.get(i));
         }
         return null;
+    }
 
+    public static void Done(String DeleteId) {
+        if (isNumer(DeleteId)) {
+            int id = Integer.parseInt(DeleteId);
+            id = id - 1;
+            if (id < 0) {
+                id = 0;
+            }
+            my_toDo.remove(id);
+        }
+    }
+
+    public static boolean isNumer(String str) {
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
